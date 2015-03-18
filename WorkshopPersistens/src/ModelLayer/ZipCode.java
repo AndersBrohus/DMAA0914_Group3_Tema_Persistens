20ae5ff51afd9951566c76b43fec7dffc07e6d93
AndersBrohus <andez@live.dk>
2015-03-18 09:36:09 +0100

    First Try

---
+++ WorkshopPersistens/src/ModelLayer/ZipCode.java
@@ -0,0 +1,49 @@
+package ModelLayer;
+
+public class ZipCode {
+	private int zipCode;
+	private String city;
+	
+	public ZipCode(int zipCode)
+	{
+		this.zipCode = zipCode;
+	}
+	
+	public ZipCode(int zipCode, String city)
+	{
+		this.zipCode = zipCode;
+		this.city = city;
+	}
+
+	/**
+	 * @return the zipCode
+	 */
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	/**
+	 * @param zipCode the zipCode to set
+	 */
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	
+	
+}
